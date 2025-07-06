54bf862defc32dd8c603ae130a4fb029e2592f44
Jim Knowler <jim.knowler2@gmail.com>
2025-07-06 20:17:23 +0100

    Arduino: init digital pins as input/output and set default values to outputs

--- BreadboardZ80.ino
+++ BreadboardZ80.ino
@@ -10,7 +10,7 @@ const int PinsDataBus[8] = {
   34, 36, 30, 24, 22, 26, 28, 32
 };
 
-const int PinCLK_N = 2;
+const int PinCLK = 2;
 const int PinINT_N = 3;
 const int PinNMI_N = 4;
 const int PinHALT_N = 5;
@@ -26,15 +26,86 @@ const int PinBUSACK_N = 42;
 const int PinWR_N = 40;
 const int PinRD_N = 38;
 
+// Z80 Opcodes
+const uint8_t OpcodeNOP = 0x00;
+
+// Write a value to the data bus
+void WriteDataBus(uint8_t Value)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    const uint8_t PinValue = (Value >> i) & 1;
+    digitalWrite(PinsDataBus[i], PinValue);
+  }
+}
+
+// Read the current value from the address bus
+uint16_t ReadAddressBus()
+{
+  uint16_t Value = 0;
+
+  for (int i = 0; i < 16; i++)
+  {
+    const uint8_t PinValue = digitalRead(PinsAddressBus[i]);
+    Value |= (PinValue < i);
+  }
+
+  return Value;
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println("BreadboardZ80");
 
+  // Read from Address 
+  for (const int Pin: PinsAddressBus) 
+  {
+    pinMode(Pin, INPUT);
+  }
   
+  // Write to Data Bus
+  for (const int Pin: PinsDataBus)
+  {
+    pinMode(Pin, OUTPUT);
+  }
+
+  WriteDataBus(OpcodeNOP);
+
+  // Write CLK
+  pinMode(PinCLK, OUTPUT);
+  digitalWrite(PinCLK, 0);
+
+  // Write INT, NMI - to disable them
+  pinMode(PinINT_N, OUTPUT);
+  digitalWrite(PinINT_N, 1);
+  pinMode(PinNMI_N, OUTPUT);
+  digitalWrite(PinNMI_N, 1);
+
+  // Read HALT, MREQ, IORQ
+  pinMode(PinHALT_N, INPUT);
+  pinMode(PinMREQ_N, INPUT);
+  pinMode(PinIORQ_N, INPUT);
+
+  // Read RFSH, M1
+  pinMode(PinRFSH_N, INPUT);
+  pinMode(PinM1_N, INPUT);
+
+  // Write RESET, BUSREQ, WAIT
+  pinMode(PinRESET_N, OUTPUT);
+  digitalWrite(PinRESET_N, 1);
+  pinMode(PinBUSREQ_N, OUTPUT);
+  digitalWrite(PinBUSREQ_N, 1);
+  pinMode(PinWAIT_N, OUTPUT);
+  digitalWrite(PinWAIT_N, 1);
+
+  // Read BUSACK
+  pinMode(PinBUSACK_N, INPUT);
+  pinMode(PinWR_N, INPUT);
+  pinMode(PinRD_N, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
